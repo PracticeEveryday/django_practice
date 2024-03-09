798e514743bb1cfc62f858f9e195904c8cf1d01d
PracticeEveryday <developerforus@gmail.com>
2024-03-09 12:20:00 +0900

    feat: add django view

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     # 쟝고가 가지고 있는 어드민 URL이 있음.
     path('admin/', admin.site.urls),
+    path('api/', include('profiles_api.urls'))
 ]
 
