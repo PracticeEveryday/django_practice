12948960a8acab3c1c12ad6994462f69f0634f8a
PracticeEveryday <developerforus@gmail.com>
2024-03-10 13:22:15 +0900

    feat: testcode

--- profiles_api/tests.py
+++ profiles_api/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rest_framework import status
 
-# Create your tests here.
+client = Client()
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.url = '/api/profile/'
+
+    # 함수명 앞에 test_가 있어야 테스트가 실행된다.
+    def test_프로필_가져오기(self):
+        response = client.get(self.url, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
